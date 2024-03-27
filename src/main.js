c9a460f3db79b098e5adce12c7add44e57110483
Milana-Ro <milana.roslyak@gmail.com>
2024-03-27 09:48:38 +0200

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,1 +1,7 @@
 console.log('main');
+
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
+
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
